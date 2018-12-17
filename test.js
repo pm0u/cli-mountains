1a21bf57e50dc888310f98fd2f988e058428ddf8
Paul Mourer <paul.mourer@gmail.com>
2018-12-16 21:45:54 -0700

    Working first prototype
    pretty much there - randomly chooses a photo from first 500 from flickr.
    photos are pretty hit or miss in terms of usability.. used jimp to add
    some brightness & contrast.. sorta helps.

--- test.js
+++ test.js
@@ -2,16 +2,18 @@ const imageToAscii = require("image-to-ascii")
 const computeSize = require('compute-size')
 const exec = require('child_process').exec
 const axios = require('axios')
+const jimp = require('jimp')
+require('dotenv').config()
 const flickrKey = process.env.flickr_key
 const flickrSecret = process.env.flickr_secret
 
 exec('tput cols', (error, stdout, stderr) => {
 
-  const termWidth = Math.min(Math.floor(stdout / 2), 60)
+  const termWidth = Math.min(Math.floor(stdout / 2), 200)
 
   const opts = {
     //bg:true,
-    white_bg:true,
+    white_bg: true,
     size_options: {
       screen_size: {
         width: termWidth
@@ -22,33 +24,30 @@ exec('tput cols', (error, stdout, stderr) => {
     },
   }
 
-  axios.get("http://api.flickr.com/services/feeds/photos_public.gne?tags=mountains&format=json").then(data => {
-    const betterData = data.data.replace(/^.*\(/,"").replace(/\)$/,"")
+  axios.get(`https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${flickrKey}&text=mountain&format=json&sort=interestingness-desc&per_page=500`).then(data => {
+    const betterData = data.data.replace(/^.*Api\(/, "").replace(/\)$/, "")
     const jsonData = JSON.parse(betterData)
-    const imageData = jsonData.items
-    imageData.forEach(datas => {
-      imageToAscii(datas.media.m, opts, (err,converted) => {
-        console.log(err || converted)
+    const photos = jsonData.photos.photo
+    const urls = []
+    photos.forEach(photo => {
+      const farmId = photo.farm
+      const serverId = photo.server
+      const id = photo.id
+      const secret = photo.secret
+      const url = `https://farm${farmId}.staticflickr.com/${serverId}/${id}_${secret}.jpg`
+      urls.push(url)
+    })
+    const randomUrl = urls[Math.floor(Math.random() * 500)]
+    jimp.read(randomUrl).then(image => {
+      image.brightness(.1)
+      image.contrast(.2)
+      image.getBuffer('image/jpeg', (err, buffer) => {
+        imageToAscii(buffer, opts, (err, converted) => {
+          console.log(err || converted)
+        })
       })
+
     })
   }).catch(err => {
     console.log(err)
   })
-
-
-  imageToAscii("https://cdn.images.express.co.uk/img/dynamic/130/590x/Mountains-625882.jpg", opts, (err, converted) => {
-    console.log(err || converted)
-    console.log('---')
-  })
-
-
-  imageToAscii("https://upload.wikimedia.org/wikipedia/commons/thumb/6/6e/Monasterio_Khor_Virap%2C_Armenia%2C_2016-10-01%2C_DD_25.jpg/1200px-Monasterio_Khor_Virap%2C_Armenia%2C_2016-10-01%2C_DD_25.jpg", opts, (err, converted) => {
-  console.log(err || converted);
-  })
-
-  //imageToAscii("https://cdn.images.express.co.uk/img/dynamic/130/590x/Mountains-625882.jpg",opts, (err, converted) => {
-  //    console.log(err || converted);
-  //})
-
-
-})
